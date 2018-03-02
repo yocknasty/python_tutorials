ed6648ef995e28bec748c4a451a3721a2abb7d20
yocknasty <ajyockey@gmail.com>
2018-03-01 18:55:48 -0800

    panit app compleated

---
+++ paint.py
@@ -0,0 +1,35 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+from random import random
+
+class MyPaintWidget(Widget):
+    def on_touch_down(self, touch):
+         color = (random(), random(), random())
+         with self.canvas:
+            Color(*color, mode='hsv')
+            d = 60.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=20.)
+        
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+
+class MyPaintApp(App):
+    def build(self):
+        parent = Widget()
+        self.painter = MyPaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+    
+    
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
+
+if __name__ == '__main__':
+    MyPaintApp().run()
